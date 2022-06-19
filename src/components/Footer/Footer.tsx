fc64126446007a49c4755299982c355212961cee
MaryWylde <marykhachatryan13@gmail.com>
2022-06-19 17:36:15 +0400

    adding folders and files

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Footer.module.scss";
+
+const Footer = () => {
+    <footer>
+        I am a footer lol
+    </footer>
+};
+
+export default Footer;
---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./NavBar.module.scss";
+
+const NavBar = () => {
+    <div>
+        I am a Header lol
+    </div>
+};
+
+export default NavBar;
+
+
